7236c4a9b11bde30a21d629b56b5d88830e43a23
DangHynh <s4045607@rmit.edu.vn>
2024-09-11 23:04:56 +0700

    add back nvim from nvim.bak

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+
+    config = function()
+        --add treesitter configs
+        local configs = require("nvim-treesitter.configs");
+        configs.setup({
+            auto_install = true,
+            ensure_installed = { "c", "lua", "javascript", "html", "css", "python", },
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
+        )
+    end
+}
