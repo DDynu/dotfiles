246b4812db063fcfe71977a10ac721ebe3e6fa61
Ddynu <khoadang826@gmail.com>
2025-02-07 19:16:15 +0700

    add live previews to nvim plugins

---
+++ .config/nvim/lua/plugins/live-previews.lua
@@ -0,0 +1,13 @@
+return {
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+        -- You can choose one of the following pickers
+        'nvim-telescope/telescope.nvim',
+        'ibhagwan/fzf-lua',
+        'echasnovski/mini.pick',
+    },
+
+	config = function ()
+		require('livepreview.config').set()
+	end
+}
