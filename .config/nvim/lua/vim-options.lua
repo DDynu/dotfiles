1fe110da966376476b7f13066593ade10513dfc7
Ddynu <khoadang826@gmail.com>
2025-02-17 21:18:28 +0700

    add comment to vim options for autosave

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,3 +5,11 @@ vim.cmd("set softtabstop=0 noexpandtab")
 vim.cmd("set shiftwidth=4")
 vim.cmd("autocmd BufNewFile,BufRead * setlocal formatoptions-=ro") -- Don't add comment after newline
 vim.cmd("autocmd BufReadPost,FileReadPost,BufNewFile * call system(\"tmux rename-window \" . expand(\"%\"))")
+
+-- For autosave upon exiting insert
+vim.o.autowriteall = true
+vim.api.nvim_create_autocmd({ 'InsertLeavePre', 'TextChanged', 'TextChangedP' }, {
+    pattern = '*', callback = function()
+        vim.cmd('silent! write')
+    end
+})
