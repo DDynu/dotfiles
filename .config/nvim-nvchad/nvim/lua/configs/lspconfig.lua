efe18ade69c28e503381785abb5b423a7e0e453f
Ddynu <khoadang826@gmail.com>
2025-05-21 01:02:12 +0700

    changed tmux theme, added pyight lsp{

--- .config/nvim-nvchad/nvim/lua/configs/lspconfig.lua
+++ .config/nvim-nvchad/nvim/lua/configs/lspconfig.lua
@@ -4,18 +4,16 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "lua_ls", "clangd",
-  pyright = {},
-}
+local servers = { "html", "cssls", "ts_ls", "lua_ls", "clangd", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup {
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	}
 end
 
 -- configuring single server, example: typescript
