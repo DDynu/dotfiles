858e9a312f2c26a8124cc22fc0a4bd3515df5e39
DDynu <khoadang826@gmail.com>
2026-01-13 17:46:54 +0700

    change theme, add tailwindcss

--- .config/nvim-nvchad/nvim/lua/configs/lspconfig.lua
+++ .config/nvim-nvchad/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "lua_ls", "clangd", "vtsls", "gdscript" }
+local servers = { "html", "cssls", "pyright", "lua_ls", "clangd", "vtsls", "gdscript", "tailwindcss" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
